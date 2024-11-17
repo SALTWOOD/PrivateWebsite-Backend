8d5cd8f6b20d6164565b049acd45dc786ef83b7f
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-11-17 23:29:44 +0800

    fix: 修复获取用户失败

--- src/routes/RouteUser.ts
+++ src/routes/RouteUser.ts
@@ -1,11 +1,16 @@
 import { UserEntity } from "../database/UserEntity.js";
 import JwtHelper from "../JwtHelper.js";
 import { RouteFactory } from "./RouteFactory.js";
+import { Request, Response } from "express";
 
 export class RouteUser {
     public static register(inst: RouteFactory) {
-        inst.app.get("/api/user", async (req, res) => {
+        inst.app.get("/api/user", async (req: Request, res: Response) => {
             const obj = JwtHelper.instance.verifyToken(req.cookies.token, "user");
+            if (!obj) {
+                res.status(401).json({ message: "Unauthorized" });
+                return;
+            }
             const id = (obj as { userId: number }).userId || -1;
             const user = inst.db.getEntity(UserEntity, id);
             if (user) {
