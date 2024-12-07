5fa7149175f1f48d3bc42f3aa012597e9535d008
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-12-07 18:38:53 +0800

    feat: 选择性获取通知

--- src/routes/RouteNotification.ts
+++ src/routes/RouteNotification.ts
@@ -6,39 +6,43 @@ import { RouteFactory } from "./RouteFactory.js";
 
 export class RouteNotification {
     public static VARIABLE: string = "comment";
-    public static SQL_WHERE_CLAUSE = `
-    comment.user != ? AND (
-        (
-            EXISTS (
-                SELECT 1
-                FROM comments parent
-                WHERE parent.id = comment.parent
-                AND parent.user = ?
-            )
-            AND comment.createdAt > ?
-        ) OR (
-            EXISTS (
-                SELECT 1
-                FROM articles article
-                WHERE article.id = comment.article
-                AND article.author = ?
+    public static getSql(getAll: boolean = false) {
+        return `
+        comment.user != ? AND (
+            (
+                EXISTS (
+                    SELECT 1
+                    FROM comments parent
+                    WHERE parent.id = comment.parent
+                    AND parent.user = ?
+                )
+            ) OR (
+                EXISTS (
+                    SELECT 1
+                    FROM articles article
+                    WHERE article.id = comment.article
+                    AND article.author = ?
+                )
             )
         )
-    )`;
+        ${getAll? "" : "AND comment.created > ?"}`;
+    }
 
-    public static getParams(user: UserEntity) {
-        return [user.id, user.id, user.lastRead, user.id];
+    public static getParams(user: UserEntity, getAll: boolean = false) {
+        if (getAll) return [user.id, user.id, user.id];
+        return [user.id, user.id, user.id, user.lastRead];
     }
 
     public static register(inst: RouteFactory) {
         inst.app.get("/api/notifications", async (req, res) => {
+            const all = String(req.query.all || "false").toLowerCase() === "true";
             const user = await Utilities.getUser(req, inst.db);
             if (!user) {
                 res.status(401).send("Unauthorized");
                 return;
             }
 
-            const notifications = await inst.db.select<Comment>(Comment, ["*"], this.SQL_WHERE_CLAUSE, this.getParams(user), this.VARIABLE);
+            const notifications = await inst.db.select<Comment>(Comment, ["*"], this.getSql(all), this.getParams(user, all), this.VARIABLE);
 
             res.json(notifications);
         });
@@ -50,7 +54,7 @@ export class RouteNotification {
                 return;
             }
 
-            const count = await inst.db.count<Comment>(Comment, this.SQL_WHERE_CLAUSE, this.getParams(user), this.VARIABLE);
+            const count = await inst.db.count<Comment>(Comment, this.getSql(), this.getParams(user), this.VARIABLE);
             res.json({ count });
         });
 
